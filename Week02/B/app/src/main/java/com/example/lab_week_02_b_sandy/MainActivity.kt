832c16899fe2451f61ce970613f0f367b980b6b4
DDWVT <dewa.vito2005@gmail.com>
2025-09-10 07:51:40 +0700

    commit no 2 - result activity [intent]

--- Week02/B/app/src/main/java/com/example/lab_week_02_b_sandy/MainActivity.kt
+++ Week02/B/app/src/main/java/com/example/lab_week_02_b_sandy/MainActivity.kt
@@ -1,12 +1,25 @@
 package com.example.lab_week_02_b_sandy
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.textfield.TextInputEditText
 
 class MainActivity : AppCompatActivity() {
+    companion object {
+        private const val COLOR_KEY = "COLOR_KEY"
+        private const val TAG = "WARNA"
+    }
+
+    private val submitBtn: Button
+        get() = findViewById(R.id.submit_btn)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -15,6 +28,31 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        submitBtn.setOnClickListener {
+            val colorCode =
+                findViewById<TextInputEditText>(R.id.color_code_input_field).text.toString()
+            Log.d(TAG, colorCode)
+
+            if (colorCode.isNotEmpty()) {
+                if (colorCode.length < 6) {
+                    Toast.makeText(
+                        this,
+                        getString(R.string.color_code_input_wrong_length),
+                        Toast.LENGTH_LONG
+                    ).show()
+                } else {
+                    val ResultIntent = Intent(applicationContext, ResultActivity::class.java)
+                    Log.d(TAG, "2 $colorCode")
+                    ResultIntent.putExtra(COLOR_KEY, colorCode)
+                    startActivity(ResultIntent)
+                    Log.d(TAG, "ada woi")
+                }
+            } else {
+                Toast.makeText(this, getString(R.string.color_code_input_empty), Toast.LENGTH_LONG)
+                    .show()
+            }
         }
     }
 }
---
+++ Week02/B/app/src/main/java/com/example/lab_week_02_b_sandy/ResultActivity.kt
@@ -0,0 +1,40 @@
+package com.example.lab_week_02_b_sandy
+
+
+import android.content.Intent
+import android.graphics.Color
+import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class ResultActivity : AppCompatActivity() {
+    companion object {
+        private const val COLOR_KEY = "COLOR_KEY"
+        private const val TAG = "RESULT"
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_result)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.bg_screen)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        if (intent != null) {
+            val colorCode = intent.getStringExtra(COLOR_KEY)
+            Log.d(TAG, "kocak: $colorCode.toString()")
+            val bgScreen = findViewById<ConstraintLayout>(R.id.bg_screen)
+            bgScreen.setBackgroundColor(Color.parseColor("#$colorCode"))
+            val resMsg = findViewById<TextView>(R.id.color_code_result_msg)
+            resMsg.text = getString(R.string.color_code_result_msg, colorCode?.uppercase())
+        }
+    }
+}
