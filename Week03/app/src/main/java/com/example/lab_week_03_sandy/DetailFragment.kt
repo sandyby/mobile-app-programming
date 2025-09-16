777cf1480a65e875f8e46df181540201cccb0c61
DDWVT <dewa.vito2005@gmail.com>
2025-09-17 04:47:02 +0700

    commit no.3 - used dynamic fragment

--- Week03/app/src/main/java/com/example/lab_week_03_sandy/DetailFragment.kt
+++ Week03/app/src/main/java/com/example/lab_week_03_sandy/DetailFragment.kt
@@ -16,6 +16,15 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class DetailFragment : Fragment() {
+    companion object {
+        private const val COFFEE_ID = "COFFEE_ID"
+        fun newInstance(coffeeId: Int) = DetailFragment().apply {
+            arguments = Bundle().apply {
+                putInt(COFFEE_ID, coffeeId)
+            }
+        }
+    }
+
     private var param1: String? = null
     private var param2: String? = null
 
@@ -39,39 +48,28 @@ class DetailFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_detail, container, false)
     }
 
-    fun setCoffeeData(id: Int){
-        when(id){
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val coffeeId = arguments?.getInt(COFFEE_ID, 0) ?: 0
+        setCoffeeData(coffeeId)
+    }
+
+    fun setCoffeeData(id: Int) {
+        when (id) {
             R.id.affogato -> {
                 coffeeTitle?.text = getString(R.string.affogato_title)
                 coffeeDesc?.text = getString(R.string.affogato_desc)
             }
+
             R.id.americano -> {
                 coffeeTitle?.text = getString(R.string.americano_title)
                 coffeeDesc?.text = getString(R.string.americano_desc)
             }
+
             R.id.latte -> {
                 coffeeTitle?.text = getString(R.string.latte_title)
                 coffeeDesc?.text = getString(R.string.latte_desc)
             }
         }
-    }
-
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment DetailFragment.
-         */
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            DetailFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
     }
 }
