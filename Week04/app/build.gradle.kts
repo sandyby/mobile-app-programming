1c848e85edfee32554fb09d8c5e6af2be49e16d4
Reen000 <alexandriamaureen8@gmail.com>
2025-09-24 20:16:02 +0700

    commit no. 1 - navigation drawer

--- Week04/app/build.gradle.kts
+++ Week04/app/build.gradle.kts
@@ -45,4 +45,6 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    implementation("androidx.navigation:navigation-ui-ktx:2.9.4")
+    implementation("androidx.navigation:navigation-fragment-ktx:2.9.4")
 }
---
+++ Week04/app/src/main/java/com/example/lab_week_04_sandy/DetailFragment.kt
@@ -0,0 +1,52 @@
+package com.example.lab_week_04_sandy
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+
+class DetailFragment : Fragment() {
+    private val coffeeTitle: TextView?
+        get() = view?.findViewById(R.id.coffee_title)
+    private val coffeeDesc: TextView?
+        get() = view?.findViewById(R.id.coffee_desc)
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_detail, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val coffeeId = arguments?.getInt(COFFEE_ID, 0) ?: 0
+        setCoffeeData(coffeeId)
+    }
+
+    fun setCoffeeData(id: Int) {
+        when (id) {
+            R.id.affogato -> {
+                coffeeTitle?.text = getString(R.string.affogato_title)
+            }
+
+            R.id.americano -> {
+                coffeeTitle?.text = getString(R.string.americano_title)
+            }
+
+            R.id.latte -> {
+                coffeeTitle?.text = getString(R.string.latte_title)
+            }
+        }
+    }
+
+    companion object {
+        private const val COFFEE_ID = "COFFEE_ID"
+    }
+}
