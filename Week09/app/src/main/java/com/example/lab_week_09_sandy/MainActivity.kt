66cb2a338b7ee0d1b4ca2e58ec27ebe9893bdc24
sandyby <sandyby56@gmail.com>
2025-11-20 14:09:09 +0700

    commit no. 1 - building jetpack compsoe ui

---
+++ Week09/app/src/main/java/com/example/lab_week_09_sandy/MainActivity.kt
@@ -0,0 +1,91 @@
+package com.example.lab_week_09_sandy
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.lab_week_09_sandy.ui.theme.LAB_WEEK_09_SandyTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            LAB_WEEK_09_SandyTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    val list = listOf("Tanu", "Tina", "Tono")
+                    Home(list)
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Home(
+    items: List<String>,
+){
+    LazyColumn {
+        item {
+            Column(
+                modifier = Modifier.padding(16.dp).fillMaxSize(),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text(text = stringResource(R.string.enter_item))
+                TextField(
+                    value = "",
+                    keyboardOptions = KeyboardOptions(
+                        keyboardType = KeyboardType.Number
+                    ),
+                    onValueChange = {
+
+                    }
+                )
+                Button(
+                    onClick = {}
+                ) {
+                    Text(text = stringResource(
+                        R.string.button_click
+                    ))
+                }
+            }
+        }
+        items(items) {
+            item ->
+            Column(
+                modifier = Modifier.padding(vertical = 4.dp).fillMaxSize(),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text(text = item)
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun HomePreview(){
+    Home(listOf("Tanu", "Tina","Tono"))
+}
