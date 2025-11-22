9b6cb93d86f3229a1042078400a210cc0b6c924a
sandyby <sandyby56@gmail.com>
2025-11-22 21:35:09 +0700

    commit no.2 - added first foreground service

--- Week08/app/src/main/java/com/example/lab_week_08_sandy/MainActivity.kt
+++ Week08/app/src/main/java/com/example/lab_week_08_sandy/MainActivity.kt
@@ -1,9 +1,13 @@
 package com.example.lab_week_08_sandy
 
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.work.Constraints
@@ -11,10 +15,14 @@ import androidx.work.Data
 import androidx.work.NetworkType
 import androidx.work.OneTimeWorkRequest
 import androidx.work.WorkManager
+import com.example.lab_week_08_sandy.service.NotificationService
 import com.example.lab_week_08_sandy.worker.FirstWorker
 import com.example.lab_week_08_sandy.worker.SecondWorker
 
 class MainActivity : AppCompatActivity() {
+    companion object {
+        const val EXTRA_ID = "Id"
+    }
     private val workManager = WorkManager.getInstance(this)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,6 +32,12 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            if (checkSelfPermission(android.Manifest.permission.POST_NOTIFICATIONS) != PackageManager.PERMISSION_GRANTED) {
+                requestPermissions(arrayOf(android.Manifest.permission.POST_NOTIFICATIONS), 1)
+            }
+        }
+
         val networkConstraints = Constraints.Builder()
             .setRequiredNetworkType(NetworkType.CONNECTED)
             .build()
@@ -49,6 +63,7 @@ class MainActivity : AppCompatActivity() {
         workManager.getWorkInfoByIdLiveData(secondRequest.id).observe(this) { info ->
             if (info?.state?.isFinished == true) {
                 showResult("Second process is done")
+                launchNotificationService()
             }
         }
     }
@@ -58,5 +73,18 @@ class MainActivity : AppCompatActivity() {
 
     private fun showResult(message: String) {
         Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+    }
+
+    private fun launchNotificationService(){
+        NotificationService.trackingCompletion.observe(
+            this
+        ) {
+            Id -> showResult("Process for Notification Channel ID $Id is done!")
+        }
+
+        val serviceIntent = Intent(this, NotificationService::class.java).apply {
+            putExtra(EXTRA_ID, "001")
+        }
+        ContextCompat.startForegroundService(this,serviceIntent)
     }
 }
