d6d127cc16947b4eae042bf6b74999be38247d8f
sandyby <sandyby56@gmail.com>
2025-11-26 16:35:42 +0700

    recommit no. 1 - implemented viewmodel to handle rotation

--- Week10/app/src/main/java/com/example/lab_week_10_sandy/MainActivity.kt
+++ Week10/app/src/main/java/com/example/lab_week_10_sandy/MainActivity.kt
@@ -3,34 +3,32 @@ package com.example.lab_week_10_sandy
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import androidx.lifecycle.ViewModelProvider
 import com.example.lab_week_10_sandy.viewmodels.TotalViewModel
-import kotlin.lazy
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var viewModel: TotalViewModel
+    private val viewModel by lazy {
+        ViewModelProvider(this)[TotalViewModel::class.java]
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        viewModel = ViewModelProvider(this).get(TotalViewModel::class.java)
         prepareViewModel()
     }
 
     private fun prepareViewModel() {
-        viewModel.total.observe(this) { total ->
-            updateText(total)
-        }
+        updateText(viewModel.total)
 
         findViewById<Button>(R.id.btn_increment).setOnClickListener {
             viewModel.incrementTotal()
+            updateText(viewModel.total)
         }
     }
 
     private fun updateText(total: Int) {
-        findViewById<TextView>(R.id.tv_total).text = getString(R.string.tv_total, total)
+        findViewById<TextView>(R.id.tv_total).text =
+            getString(R.string.tv_total, total)
     }
 }
