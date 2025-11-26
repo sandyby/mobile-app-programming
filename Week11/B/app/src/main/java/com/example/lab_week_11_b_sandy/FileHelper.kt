440ceebab5474c8d77f96266a9b0127cfb859384
sandyby <sandyby56@gmail.com>
2025-11-27 03:46:31 +0700

    commit no. 1 - integrating image and video captures to gallery using fileprovider and mediastore

---
+++ Week11/B/app/src/main/java/com/example/lab_week_11_b_sandy/FileHelper.kt
@@ -0,0 +1,18 @@
+package com.example.lab_week_11_b_sandy
+
+import android.content.Context
+import android.net.Uri
+import android.os.Environment
+import androidx.core.content.FileProvider
+import java.io.File
+
+class FileHelper(private val context: Context) {
+    fun getUriFromFile(file: File): Uri {
+        return FileProvider.getUriForFile(
+            context, "com.example.lab_week_11_b_sandy.camera", file
+        )
+    }
+
+    fun getPicturesFolder(): String = Environment.DIRECTORY_PICTURES
+    fun getVideosFolder(): String = Environment.DIRECTORY_MOVIES
+}
